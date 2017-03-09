97c19bb6e34f42b9e3e66083f38e8b648aa6297d
slindquist3 <slindquist3@gmail.com>
2017-03-08 23:36:09 -0500

    fixed root_reducer typo

--- src/reducers/reducer_products.js
+++ src/reducers/reducer_products.js
@@ -5,7 +5,6 @@ export default function(state = [], action) {
   switch(action.type) {
     case FETCH_PRODUCTS:
       return [ action.payload.data.data, ...state ]
-      // what should this be returning so that cart gets this product?
   }
   return state;
 }
